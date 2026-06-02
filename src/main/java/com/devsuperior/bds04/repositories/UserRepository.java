cfb37654fff5d967ac03b7d02abfa92c51abcd1c
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-06-01 21:19:49 -0300

    Criação da classe na pasta projections: UserDetailsProjection, criação da classe na pasta services: UserService e atualização da classe: UserRepository

--- src/main/java/com/devsuperior/bds04/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/bds04/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.devsuperior.bds04.repositories;
 
 import com.devsuperior.bds04.entities.User;
+import com.devsuperior.bds04.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
